602eaba9f539a3ff41551b8b1e4065099e307989
Anders Folkesson <anders.folkesson@gu.se>
2024-12-06 09:23:49 +0100

    fixed with connection

--- omero_funcs.py
+++ omero_funcs.py
@@ -1,82 +1,121 @@
 import omero
 from omero.cli import CLI
-from omero.gateway import BlitzGateway
+from omero.gateway import BlitzGateway, ProjectWrapper
 import ezomero
-import logging
-import config
+import omero.rtypes
+import logger
 
-logger = logging.getLogger(config.APP_NAME)
 
-#OMERO functions
-def create_omero_connection(host, session_key):
-    session_key = session_key
-    host = host
-        
-    # Create a client using the session key
-    client = omero.clients.BaseClient(host)
-    client.joinSession(session_key)
-        
-    # Create a BlitzGateway connection using the client
-    conn = BlitzGateway(client_obj=client)
-    return conn
+#the main connection object (BlitzGaterway). This should really be wrapped in a class
+theConn = None
+theHostname = None
+theToken = None
+
+def get_omero_connection():
+    return theConn
 
 def connect_to_omero(hostname, port, token):
-    logger.info("Enter function connect_to_omero")
-    cli = CLI()
-    cli.loadplugins()
+    global theHostname
+    theHostname = hostname
     
-    login_args = ["login", "-s", hostname, "-k", token, "-p", port]
+    global theToken
+    theToken= token
     
-    try:
-        cli.invoke(login_args)
-    except Exception as e:
-        logger. info(e)
-        return None, None
+    global theConn
+    theConn = BlitzGateway(host=hostname,port=port)
+    isConn = theConn.connect(token)
     
-    event_context = cli.get_event_context()
-    
-    if event_context is None:
-        logger.info("Login failed: No event context returned.")
-        return None, None
-    
-    logger.info("login done")
-    session_key = event_context.sessionUuid
-    
-    logger.info("connection done")
-    return session_key, hostname
+    return hostname, token, isConn
 
 
-def get_or_create_project(conn, project_name, verbose=False):
-    if verbose:
-        print("Setting or grabbing the Project")
+
+# def connect_to_omero(hostname, port, token):
+#     logger.info("Enter function connect_to_omero")
+#     cli = CLI()
+#     cli.loadplugins()
+    
+#     login_args = ["login", "-s", hostname, "-k", token, "-p", port]
+    
+#     try:
+#         cli.invoke(login_args)
+#     except Exception as e:
+#         logger. info(e)
+#         return None, None
+    
+#     event_context = cli.get_event_context()
+    
+#     if event_context is None:
+#         logger.info("Login failed: No event context returned.")
+#         return None, None
+    
+#     logger.info("login done")
+#     session_key = event_context.sessionUuid
+    
+#     logger.info("connection done")
+#     cli = cli.getClient()
+#     conn =BlitzGateway(client_obj=cli)
+#     return session_key, hostname, conn
+
+
+def get_user_project_if_it_exists(conn, project_name):
+    projects = get_user_projects(conn)
+    for p in projects:
+        if p.getName() == project_name:
+            return p
+        
+    return None
+    
+
+def get_or_create_project(conn, project_name):
+    logger.debug("Setting or grabbing the Project")
 
     # Try to get the project
-    projects = list(conn.getObjects("Project", attributes={"name": project_name}))
-    
-    if projects:
-        project = projects[0]
-        projID = project.getId()
-        if verbose:
-            print(f"Using existing project - ID: {projID}, Name: {project.getName()}")
-    else:
+    project = get_user_project_if_it_exists(conn, project_name)
+        
+    if not project:
         # Create project using UpdateService
-        project = omero.model.ProjectI()
-        project.setName(omero.rtypes.rstring(project_name))
-        project = conn.getUpdateService().saveAndReturnObject(project)
-        projID = project.getId().getValue()
-        if verbose:
-            print(f"Created new project - ID: {projID}, Name: {project_name}")
+        p = omero.model.ProjectI()
+        project = ProjectWrapper(obj=p,conn=conn)
+        project.setName(project_name)
+        project.save()
+        logger.info(f"Created new project - ID: {project.getId()}, Name: {project_name}")
     
-    return projID
+    
+    return project.getId()
 
-def get_user_projects(conn):
+def get_logged_in_user_name(conn):
+    return conn.getUser().getName()
+
+def get_user_project_ids(conn):
     projects = []
-    for p in conn.listProjects():         # Initially we just load Projects
+    my_expId = conn.getUser().getId()
+    for p in conn.listProjects(my_expId):         # Initially we just load Projects
         projects.append((p.getName(),p.getId()))
         
     return projects
     
-def get_datasets_for_projects(conn, project_id):
+def get_user_projects(conn):
+    projects = []
+    my_expId = conn.getUser().getId()
+    for p in conn.listProjects(my_expId):         # Initially we just load Projects
+        projects.append(p)
+        
+    return projects
+
+    
+def get_dataset_for_projects(conn, project_id):
+    project = conn.getObject("Project", project_id)
+    if not project:
+        raise Exception(f"Project with ID {project_id} not found")
+
+    datasets = []
+    for dataset in project.listChildren():      # lazy-loading of Datasets here
+        datasets.append(dataset)
+
+    return datasets
+
+
+def get_dataset_ids_for_projects(conn, project_id):
     project = conn.getObject("Project", project_id)
     if not project:
         raise Exception(f"Project with ID {project_id} not found")
@@ -88,49 +127,48 @@ def get_datasets_for_projects(conn, project_id):
     return datasets
 
 
-def get_or_create_dataset(conn, project_id, dataset_name, verbose=False):
+def get_or_create_dataset(conn, project_id, dataset_name):
     project = conn.getObject("Project", project_id)
     if not project:
         raise Exception(f"Project with ID {project_id} not found")
 
-    for dataset in project.listChildren():
-        if dataset.getName() == dataset_name:
-            if verbose:
-                print(f"Dataset '{dataset_name}' already exists in project. Using existing dataset.")
-            return dataset.getId()
 
-    # Dataset doesn't exist, create it
-    dataset = omero.model.DatasetI()
-    dataset.setName(omero.rtypes.rstring(dataset_name))
-    dataset = conn.getUpdateService().saveAndReturnObject(dataset)
-    dataset_id = dataset.getId().getValue()
+    data = [d for d in project.listChildren() if d.getName() == dataset_name]
 
-    # Link dataset to project
-    link = omero.model.ProjectDatasetLinkI()
-    link.setParent(omero.model.ProjectI(project_id, False))
-    link.setChild(dataset)
-    conn.getUpdateService().saveObject(link)
-    
-    if verbose:
-        print(f"Created new dataset '{dataset_name}' with ID {dataset_id} and linked to project.")
-    
+    if len(data) > 0:
+        dataset_id = data[0].getId()
+        logger.debug(f"Dataset '{dataset_name}' already exists in project. Using existing dataset.")
+    else:
+        # Dataset doesn't exist, create it
+        dataset = omero.model.DatasetI()
+        dataset.setName(omero.rtypes.rstring(dataset_name))
+        dataset = conn.getUpdateService().saveAndReturnObject(dataset)
+        dataset_id = dataset.getId()
+
+        # Link dataset to project
+        link = omero.model.ProjectDatasetLinkI()
+        link.setParent(omero.model.ProjectI(project_id, False))
+        link.setChild(dataset)
+        conn.getUpdateService().saveObject(link)
+        
+        logger.info(f"Created new dataset '{dataset_name}' with ID {dataset_id.getValue()} and linked to project.")
+        
     return dataset_id
 
 def import_image(conn, img_path, dataset_id, meta_dict):
     
     # import the image - work for the CCI IT!
     namespace = omero.constants.metadata.NSCLIENTMAPANNOTATION
-    image_id = ezomero.ezimport(conn,
-                                img_path,
-                                dataset=dataset_id,
+    
+    image_id = ezomero.ezimport(conn=conn,
+                                target=img_path,
+                                dataset=dataset_id.getId(),
                                 ann=meta_dict,
-                                ns=namespace,
-                                )
+                                ns=namespace)
     
     #add tag in the image as well 
     for im in image_id:
         image = conn.getObject("Image", im)
-    
         
         # Add tags
         tags = [meta_dict['Microscope'], str(meta_dict['Lens Magnification'])+"X", meta_dict['Image type']]
