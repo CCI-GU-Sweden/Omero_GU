c186d51ec2ddfe6d1f5cb83559202f6d856ce6a7
Anders Folkesson <anders.folkesson@gu.se>
2025-06-24 10:03:36 +0200

    added tests for important files and structures

--- tests/test_file_importer.py
+++ tests/test_file_importer.py
@@ -1,10 +1,19 @@
-from omerofrontend import conf
-from omerofrontend.file_data import FileData
-import os
-from omerofrontend.file_importer import FileImporter
+from datetime import datetime
+from unittest.mock import patch
 from werkzeug.datastructures import FileStorage
-import logging
+from omerofrontend.file_data import FileData
+from omerofrontend.omero_connection import OmeroConnection
+from omerofrontend import image_funcs
+from omerofrontend.file_importer import FileImporter
+from omerofrontend.temp_file_handler import TempFileHandler
+from omerofrontend.logger import logging
 
+class FakeDataset:
+    def __init__(self, value):
+        self.value = value
+        
+    def getValue(self):
+        return self.value
 
 
 class TestFileImporter:
@@ -18,29 +27,75 @@ class TestFileImporter:
     def teardown_class(cls):
         logging.getLogger().info(f"Stopping {cls.__name__}")
     
-
-    def test_store_and_remove_temp_file(self):
-        czi_path = 'tests/data/test_image.czi'
-
+    def test_file_imports_emd(self):
+        czi_path = 'tests/data/test_emd_file.emd'
         # Open the file in binary mode
         with open(czi_path, 'rb') as f:
+            username = "gunnar"
+            emd_filestorage = FileStorage(
+            stream=f,
+            filename=czi_path,           # You can set this to whatever name you want
+            content_type='application/octet-stream')  # Generic binary; adjust if you know the specific MIME type
+
+            tfh = TempFileHandler()
+            fileData = tfh.check_and_store_tempfiles([emd_filestorage], username)
+            path, metadict = image_funcs.file_format_splitter(fileData)
+            scopes = self.fi._get_scopes_metadata(metadict)
+            assert path != fileData.getMainFileTempPath()
+            assert(len(scopes) == 1)
+            assert scopes[0] == 'Talos L120C'  
+            self.fi._set_folder_and_converted_name(fileData,metadict, path)
+            assert fileData.getConvertedFileName() == "test_emd_file.ome.tiff"
+            self.do_file_imports(fileData, metadict, scopes)
+            tfh._remove_temp_files(fileData)  # Clean up temp files after test
+ 
+    def test_file_imports_czi(self):
+        czi_path = 'tests/data/test_image.czi'
+        # Open the file in binary mode
+        with open(czi_path, 'rb') as f:
+            username = "gunnar"
             czi_filestorage = FileStorage(
             stream=f,
             filename=czi_path,           # You can set this to whatever name you want
             content_type='application/octet-stream')  # Generic binary; adjust if you know the specific MIME type
 
-            filePath = os.path.join(conf.UPLOAD_FOLDER, *os.path.split(czi_path))
-            res, fpath, file_size = self.fi._store_temp_file(czi_filestorage)
-        
-            assert(res)
-            assert(fpath == filePath)
-            assert(os.path.isdir(os.path.dirname(filePath)))
-            assert(os.path.exists(filePath))
+            tfh = TempFileHandler()
+            fileData = tfh.check_and_store_tempfiles([czi_filestorage], username)
+            path, metadict = image_funcs.file_format_splitter(fileData)
+            scopes = self.fi._get_scopes_metadata(metadict)
+            assert path == fileData.getMainFileTempPath()  #should be no conversion for .czi files
+            assert(len(scopes) == 1)
+            assert(scopes[0] == "LSM 980")  
+            self.fi._set_folder_and_converted_name(fileData,metadict, path)
+            assert fileData.getConvertedFileName() == "test_image.czi"
+            self.do_file_imports(fileData, metadict, scopes)
+            tfh._remove_temp_files(fileData)  # Clean up temp files after test
 
-            fileData = FileData(czi_filestorage)
-            fileData.setTempFilePaths([fpath])
-            self.fi._remove_temp_files(fileData)
+    def do_file_imports(self, fileData: FileData, metadict, scopes):
 
-            assert(not os.path.exists(filePath))
-            assert(not os.path.isdir(os.path.dirname(filePath)))
+        conn = OmeroConnection()
+        dataset = None
+        with patch.object(conn,'get_or_create_project', return_value=55), patch.object(conn,'get_or_create_dataset', return_value=66),patch.object(conn,'getDataset', side_effect=FakeDataset):
+            now = datetime.now() # current date and time
+            date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+            dataset = self.fi._check_create_project_and_dataset_(scopes[0],date_time, conn)
+            assert(dataset is not None)
+            assert(dataset.getValue() == 66)
+            
+        fname = fileData.getConvertedFileName()    
+        with patch('omerofrontend.omero_funcs.check_duplicate_file', return_value=(False,None)), patch.object(conn,'compareImageAcquisitionTime', return_value=False):
+            isDup = self.fi._check_duplicate_file_rename_if_needed(fileData,dataset,metadict,conn)
+            assert(not isDup)
+            assert(fname == fileData.getConvertedFileName())
+
+        with patch('omerofrontend.omero_funcs.check_duplicate_file', return_value=(True,66)), patch.object(conn,'compareImageAcquisitionTime', return_value=True):
+            isDup = self.fi._check_duplicate_file_rename_if_needed(fileData,dataset,metadict,conn)
+            assert(isDup)
+            assert(fname == fileData.getConvertedFileName())
+
+            
+        with patch('omerofrontend.omero_funcs.check_duplicate_file', return_value=(True,66)), patch.object(conn,'compareImageAcquisitionTime', return_value=False):
+            isDup = self.fi._check_duplicate_file_rename_if_needed(fileData,dataset,metadict,conn)
+            assert(not isDup)
+            assert(fname != fileData.getConvertedFileName())
             
