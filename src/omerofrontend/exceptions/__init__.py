e3ad05f0b0736319d6f46feff22b32dc54ecea05
Anders Folkesson <anders.folkesson@gu.se>
2025-06-27 15:29:25 +0200

    added more exceptions

--- src/omerofrontend/exceptions/__init__.py
+++ src/omerofrontend/exceptions/__init__.py
@@ -2,3 +2,6 @@ from .exceptions import ImageNotSupported  # noqa: F401
 from .exceptions import GeneralError  # noqa: F401
 from .exceptions import DuplicateFileExists  # noqa: F401
 from .exceptions import MetaDataError  # noqa: F401
+from .exceptions import OmeroConnectionError  # noqa: F401
+from .exceptions import AssertImportError  # noqa: F401
+from .exceptions import ImportError  # noqa: F401
