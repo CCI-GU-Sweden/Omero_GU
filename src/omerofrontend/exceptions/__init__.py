8bcef36b9f5f2932e02b74d042f92f282e350160
Anders Folkesson <anders.folkesson@gu.se>
2025-07-07 10:51:15 +0200

    added out of disk error exception

--- src/omerofrontend/exceptions/__init__.py
+++ src/omerofrontend/exceptions/__init__.py
@@ -5,3 +5,4 @@ from .exceptions import MetaDataError  # noqa: F401
 from .exceptions import OmeroConnectionError  # noqa: F401
 from .exceptions import AssertImportError  # noqa: F401
 from .exceptions import ImportError  # noqa: F401
+from .exceptions import OutOfDiskError  # noqa: F401
