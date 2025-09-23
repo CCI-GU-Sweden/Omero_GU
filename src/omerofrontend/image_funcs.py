05522d7b8f102da7bbfdaa88c941d601a89ac9e5
LECLERC Simon <leclercsimon74@gmail.com>
2025-09-23 17:12:27 +0200

    Fibics tiff support added

--- src/omerofrontend/image_funcs.py
+++ src/omerofrontend/image_funcs.py
@@ -1,4 +1,5 @@
 import os
+import math
 import datetime
 import re
 from dateutil import parser
@@ -220,10 +221,11 @@ def file_format_splitter(fileData : FileData) -> tuple[str, dict[str,str]]:
     img_path = fileData.getMainFileTempPath()
     logger.info(f"Received file is of format {ext}")
     if ext == "czi": #Light microscope format - CarlZeissImage
-        converted_path = img_path
         key_pair = get_info_metadata_from_czi(Path(img_path))
-    elif ext == "tif": #Tif, but only SEM-TIF
-        converted_path, key_pair = convert_semtif_to_ometiff(img_path)
+        #TODO check if the data is coming from the LSM700 or 710, then convert to ome-tiff if bigger than 1 GB
+        converted_path = img_path
+    elif ext == "tif": #Tif, but only SEM-TIF or Fibics-TIF are supported
+        converted_path, key_pair = convert_tif_to_ometiff(img_path)
     elif ext == "mrc":
         atlasPair = {}
         atlasPair[fileData.getDictFileExtension()] = fileData.getDictFileTempPath()
@@ -846,7 +848,43 @@ def convert_atlas_to_ometiff(img_path: dict):
     return output_fpath, key_pair
 
 
-def convert_semtif_to_ometiff(img_path: str):
+def extract_tags_from_tif(img_path: str) -> dict:
+    """
+    Extract all tags from a TIFF image file.
+    Args:
+    img_path (str): Path to the TIFF image file.
+    Returns:
+    dict: A dictionary containing all extracted tags and their values.
+    """
+    with tifffile.TiffFile(img_path) as tf:
+        tif_tags = {}
+        for i, page in enumerate(tf.pages):
+            for tag in page.tags.values():
+                name = getattr(tag, "name", None) or f"TAG_{tag.code}"
+                val = tag.value
+                if isinstance(val, (bytes, bytearray)):
+                    # try to preview as text (common for custom metadata)
+                    try:
+                        preview = val.decode("utf-8", "replace")
+                    except Exception:
+                        preview = str(val)
+                else:
+                    preview = str(val)
+
+                tif_tags[name] = preview
+
+        return tif_tags
+    
+def convert_tif_to_ometiff(img_path: str):
+    tif_tags = extract_tags_from_tif(img_path)
+    if "CZ_SEM" in tif_tags:
+        return convert_semtif_to_ometiff(img_path, tif_tags)
+    elif "FibicsXML" in tif_tags:
+        return convert_fibics_to_ometiff(img_path, tif_tags)
+    else:
+        raise ImageNotSupported(img_path, "TIFF image is not a supported SEM-TIF or Fibics format.")
+
+def convert_semtif_to_ometiff(img_path: str, tif_tags: dict) -> tuple[str, dict]:
     """
     Convert SEM TIF file to ome-tiff format.
     
@@ -857,64 +895,59 @@ def convert_semtif_to_ometiff(img_path: str):
     str: Path to the output OME-TIFF file
     dict: Contains the key-pair values
     """
-    logger.debug(f"Conversion to ometiff from tif required for {img_path}")
-    with tifffile.TiffFile(img_path) as tif:
+    cz_sem_metadata = dict(tif_tags["CZ_SEM"])
         
-        sem_meta = tif.pages[0].tags.get(34118) # pyright: ignore[reportGeneralTypeIssues]
-        if sem_meta is None:
-            raise ImageNotSupported(img_path,"Image may be a tif, but not a SEM-TIF or metadata failed to be read")
-        
-        cz_sem_metadata = dict(sem_meta.value)
-        
-        logger.debug(f"{img_path} metadata successfully readen!") 
-        
-        #img_array = tif.asarray()
-        logger.debug(f"{img_path} data successfully readen!") 
-                    
-        #construct mag string
-        mag_str_list = list(map(str,dict_crawler(cz_sem_metadata, 'ap_mag')[0]))
-        mag_str = " ".join(mag_str_list)
-                            
-        key_pair = {
-        'Microscope': mapping(dict_crawler(cz_sem_metadata, 'dp_sem')[0][1]),
-        'Image type':dict_crawler(cz_sem_metadata, 'dp_final_lens')[0][1],
-        'Lens Magnification': convert_magnification(mag_str),
-        'WD value': dict_crawler(cz_sem_metadata, 'ap_wd')[0][1],
-        'WD unit': dict_crawler(cz_sem_metadata, 'ap_wd')[0][2],
-        'EHT value': dict_crawler(cz_sem_metadata, 'ap_actualkv')[0][1],
-        'EHT unit': dict_crawler(cz_sem_metadata, 'ap_actualkv')[0][2],
-        'I Probe value': dict_crawler(cz_sem_metadata, 'ap_iprobe')[0][1],
-        'I Probe unit': dict_crawler(cz_sem_metadata, 'ap_iprobe')[0][2],
-        'Signal A' : dict_crawler(cz_sem_metadata, 'dp_detector_channel')[0][1],
-        'Signal B': dict_crawler(cz_sem_metadata, 'dp_implied_detector')[0][1],
-        'Mixing' : dict_crawler(cz_sem_metadata, 'dp_mixing')[0][1],
-        'Mixing proportion' : dict_crawler(cz_sem_metadata, 'ap_k2')[0][1],
-        'Scan speed': dict_crawler(cz_sem_metadata, 'dp_scanrate')[0][1],
-        'Dwell time value': dict_crawler(cz_sem_metadata, 'dp_dwell_time')[0][1],
-        'Dwell time unit': dict_crawler(cz_sem_metadata, 'dp_dwell_time')[0][2],
-        'Line Avg.Count': dict_crawler(cz_sem_metadata, 'ap_line_average_count')[0][1],
-        'Vacuum Mode': dict_crawler(cz_sem_metadata, 'dp_vac_mode')[0][1],
-        'Mag': dict_crawler(cz_sem_metadata, 'ap_mag')[0][1],
-        'Mag Range': dict_crawler(cz_sem_metadata, 'dp_mag_range')[0][1],
-        'Image Size X' : int(dict_crawler(cz_sem_metadata, 'dp_image_store')[0][1].split(' * ')[0]),
-        'Image Size Y' : int(dict_crawler(cz_sem_metadata, 'dp_image_store')[0][1].split(' * ')[1]),
-        'Physical pixel size': dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][1],
-        'Physical pixel unit':dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][2],
-        'Physical pixel size X': dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][1],
-        'Physical pixel size Y': dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][1],
-        'Image depth': tif.pages[0].tags.get(258).value, # pyright: ignore[reportOptionalMemberAccess, reportGeneralTypeIssues]
-        'Comment': dict_crawler(cz_sem_metadata, 'sv_user_text')[0][1],
-        }
-            
-        date = dict_crawler(cz_sem_metadata, 'ap_date')[0][1]
-        time = dict_crawler(cz_sem_metadata, 'ap_time')[0][1]
-        
-        date_object = datetime.datetime.strptime(date+' '+time, "%d %b %Y %H:%M:%S")
-        date_str = date_object.strftime(conf.DATE_TIME_FMT)
-        key_pair['Acquisition date'] = date_str
+    logger.debug(f"{img_path} metadata successfully readen!") 
 
-        output_fpath = img_path
-            
+    #construct mag string
+    mag_str_list = list(map(str,dict_crawler(cz_sem_metadata, 'ap_mag')[0]))
+    mag_str = " ".join(mag_str_list)
+                        
+    key_pair = {
+    'Microscope': mapping(dict_crawler(cz_sem_metadata, 'dp_sem')[0][1]),
+    'Image type':dict_crawler(cz_sem_metadata, 'dp_final_lens')[0][1],
+    'Lens Magnification': convert_magnification(mag_str),
+    'WD value': dict_crawler(cz_sem_metadata, 'ap_wd')[0][1],
+    'WD unit': dict_crawler(cz_sem_metadata, 'ap_wd')[0][2],
+    'EHT value': dict_crawler(cz_sem_metadata, 'ap_actualkv')[0][1],
+    'EHT unit': dict_crawler(cz_sem_metadata, 'ap_actualkv')[0][2],
+    'I Probe value': dict_crawler(cz_sem_metadata, 'ap_iprobe')[0][1],
+    'I Probe unit': dict_crawler(cz_sem_metadata, 'ap_iprobe')[0][2],
+    'Signal A' : dict_crawler(cz_sem_metadata, 'dp_detector_channel')[0][1],
+    'Signal B': dict_crawler(cz_sem_metadata, 'dp_implied_detector')[0][1],
+    'Mixing' : dict_crawler(cz_sem_metadata, 'dp_mixing')[0][1],
+    'Mixing proportion' : dict_crawler(cz_sem_metadata, 'ap_k2')[0][1],
+    'Scan speed': dict_crawler(cz_sem_metadata, 'dp_scanrate')[0][1],
+    'Dwell time value': dict_crawler(cz_sem_metadata, 'dp_dwell_time')[0][1],
+    'Dwell time unit': dict_crawler(cz_sem_metadata, 'dp_dwell_time')[0][2],
+    'Line Avg.Count': dict_crawler(cz_sem_metadata, 'ap_line_average_count')[0][1],
+    'Vacuum Mode': dict_crawler(cz_sem_metadata, 'dp_vac_mode')[0][1],
+    'Mag': dict_crawler(cz_sem_metadata, 'ap_mag')[0][1],
+    'Mag Range': dict_crawler(cz_sem_metadata, 'dp_mag_range')[0][1],
+    'Image Size X' : int(dict_crawler(cz_sem_metadata, 'dp_image_store')[0][1].split(' * ')[0]),
+    'Image Size Y' : int(dict_crawler(cz_sem_metadata, 'dp_image_store')[0][1].split(' * ')[1]),
+    'Physical pixel size': dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][1],
+    'Physical pixel unit':dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][2],
+    'Physical pixel size X': dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][1],
+    'Physical pixel size Y': dict_crawler(cz_sem_metadata, 'ap_image_pixel_size')[0][1],
+    'Image depth': tif_tags.get("BitsPerSample", "8"),
+    'Comment': dict_crawler(cz_sem_metadata, 'sv_user_text')[0][1],
+    }
+        
+    date = dict_crawler(cz_sem_metadata, 'ap_date')[0][1]
+    time = dict_crawler(cz_sem_metadata, 'ap_time')[0][1]
+    
+    date_object = datetime.datetime.strptime(date+' '+time, "%d %b %Y %H:%M:%S")
+    date_str = date_object.strftime(conf.DATE_TIME_FMT)
+    key_pair['Acquisition date'] = date_str
+
+    output_fpath = img_path
+    
+    return output_fpath, key_pair
+
+    #legacy code in case we want to write the ome-tiff from semtif
+    # with tifffile.TiffFile(img_path) as tif:
+        #img_array = tif.asarray()
                                                 
         # Create an OME object
         # ome = model.OME()
@@ -988,9 +1021,101 @@ def convert_semtif_to_ometiff(img_path: str):
             
         # logger.debug(f"Ome-tiff written at {output_fpath}.")
         
-        return output_fpath, key_pair
-    
-    #raise FileNotFoundError("The file does not exist.")
+def choose_levels(y, x, target_min=1024):
+    """Choose the number of subresolutions to include in the OME-TIFF file."""
+    L = 1 + max(0, math.ceil(math.log2(max(y, x) / target_min)))
+    return max(int(L), 1)
+
+def convert_fibics_to_ometiff(img_path: str, tif_tags: dict):
+
+    #grab the data
+    with tifffile.TiffFile(img_path) as tif:
+        array = tif.asarray()
+    #prepare the output path
+    out_name = f"{Path(img_path).stem}.ome.tiff"
+    #brut force approach to extract the pixel size from the FibicsXML tag
+    FLOAT = r'[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?'
+    re_Ux = re.compile(rf"<\s*Ux\s*>\s*({FLOAT})\s*<\s*/\s*Ux\s*>", re.I)
+    re_Uy = re.compile(rf"<\s*Uy\s*>\s*({FLOAT})\s*<\s*/\s*Uy\s*>", re.I)
+    re_Vx = re.compile(rf"<\s*Vx\s*>\s*({FLOAT})\s*<\s*/\s*Vx\s*>", re.I)
+    re_Vy = re.compile(rf"<\s*Vy\s*>\s*({FLOAT})\s*<\s*/\s*Vy\s*>", re.I)
+
+    image_width = tif_tags.get("ImageWidth")
+    image_length = tif_tags.get("ImageLength")
+    xml_text = tif_tags.get("FibicsXML")
+
+    if image_width is None or image_length is None: #fallback if the tags are not present
+        image_width = array.shape[1]
+        image_length = array.shape[0]
+
+    px_um, py_um = 1.0, 1.0 #initialize with 1.0 in case the regex fail
+    mUx = re_Ux.search(xml_text) if isinstance(xml_text, str) else None
+    mUy = re_Uy.search(xml_text) if isinstance(xml_text, str) else None
+    mVx = re_Vx.search(xml_text) if isinstance(xml_text, str) else None
+    mVy = re_Vy.search(xml_text) if isinstance(xml_text, str) else None
+    if mUx and mUy and mVx and mVy:
+        Ux, Uy = float(mUx.group(1)), float(mUy.group(1))
+        Vx, Vy = float(mVx.group(1)), float(mVy.group(1))
+        px_um = math.hypot(Ux, Uy)
+        py_um = math.hypot(Vx, Vy)
+
+    # Convert µm/px -> pixels per centimeter
+    px_cm = px_um * 1e-4
+    py_cm = py_um * 1e-4
+    xres0 = 1.0 / px_cm
+    yres0 = 1.0 / py_cm
+
+    #Save the data as ome-tiff
+    subresolutions = choose_levels(float(image_width), float(image_length))
+    with tifffile.TiffWriter(out_name, bigtiff=True) as tif:
+        options = dict(photometric="minisblack",
+                        compression="zlib",
+                        predictor=True,
+                        tile = (512, 512),
+                        maxworkers=2,
+                        )
+        tif.write(
+            array,
+            subifds=subresolutions-1,
+            metadata={
+                    "axes": "YX",
+                    "PhysicalSizeX": px_um,
+                    "PhysicalSizeXUnit": "µm",
+                    "PhysicalSizeY": py_um,
+                    "PhysicalSizeYUnit": "µm",
+            },
+            resolution=(xres0, yres0),
+            resolutionunit="CENTIMETER",
+            extratags=[(51023, "s", 0, xml_text, False)],
+            **options,
+            )
+        
+        for level in range(1, subresolutions):
+            mag = 2 ** level
+            tif.write(
+                array[..., ::mag, ::mag],
+                subfiletype=1,
+                resolution=(xres0/mag, yres0/mag),
+                resolutionunit="CENTIMETER",
+                **options,
+                )
+
+    #generate a small key-value pair for Omero!
+    mini_metadata = {'Microscope':'Gemini',
+                     'Lens Magnification': math.nan,
+                     'Lens NA': math.nan,
+                     'Image type':'Gemini',
+                     'Physical pixel size X':px_um,
+                     'Physical pixel size Y':py_um,
+                     'Image Size X':image_width,
+                     'Image Size Y':image_length,
+                     'Comment':None,
+                     'Description':None,
+                     'Acquisition date': datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+                     }
+
+    return out_name, mini_metadata
+
 
 def convert_magnification(mag_str):
     """Convert magnification string like '81.20 K X' to a real number."""
